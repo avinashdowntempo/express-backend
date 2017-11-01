ab47f76e11ff74d0fc61b9cce83b3c1615251e77
Avinash Jeyasankar <avinashj@CESCHN.COM>
2017-11-01 22:16:11 +0530

    added access control header

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config');
-
+var jwt = require('jsonwebtoken');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var getToken = require('./routes/getToken');
 var app = express();
+var apiRoutes = express.Router(); 
 
 app.set('superSecret', config.secret);
 console.log(app.get('superSecret'));
@@ -30,13 +31,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin,  Authorization, Access-Control-Allow-Headers, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+
+
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/gettoken', getToken);
+apiRoutes.use(function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+      jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+  
+    } else {
+      return res.status(403).send({ 
+          success: false, 
+          message: 'No token provided.' 
+      });  
+    }
+  });
+app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
