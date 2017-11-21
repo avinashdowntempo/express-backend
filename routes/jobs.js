fae47918b3acdda86fd0b163d758f71425bb1df2
avinash-ces <avinash.jeyasankar@cesltd.com>
2017-11-21 22:41:57 +0530

    added get request to list jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -20,4 +20,17 @@ router.post('/new', function (req, res, next) {
   console.log(job);
 });
 
+router.get('/all', function (req, res, next) {
+  console.log(req.body);
+  console.log("23232", req.body.email);
+  job.find(function (err, small) {
+    if (err) {
+      console.log(err);
+      res.status(403).end();
+    }
+    res.status(200).send({ value: small, msg: "updated succesfully" });
+  });
+  console.log(job);
+});
+
 module.exports = router;
