edc94a37cee00b5848c98dc26d5ea4b5a719c68a
avinash-ces <avinash.jeyasankar@cesltd.com>
2017-11-23 20:31:01 +0530

    added job update feature

--- routes/jobs.js
+++ routes/jobs.js
@@ -20,6 +20,18 @@ router.post('/new', function (req, res, next) {
   console.log(job);
 });
 
+router.patch('/update', function (req, res, next) {
+  console.log(req.body);
+  console.log("23232", req.body._id);
+  job.update({ _id: req.body._id }, req.body, function (err, small) {
+    if (err) {
+      console.log(err);
+      res.status(403).end();
+    }
+    res.status(200).send({ value: small, msg: "updated succesfully" });
+  });
+});
+
 router.get('/all', function (req, res, next) {
   console.log(req.body);
   console.log("23232", req.body.email);
